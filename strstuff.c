43353558b5fb5875ebd277f81f9f36da5097a748
c0d3m0nk3y <rickyoswald@gmail.com>
2024-12-07 11:48:48 +0000

    Leetcode Longest Common Prefix

--- strstuff.c
+++ strstuff.c
@@ -34,15 +34,36 @@ char* longestCommonDerpfix(char** strs, int strsSize) {
 
 char* longestCommonPrefix(char** strs, int strsSize) {
   char *ret = malloc(sizeof(char) * 200);
+  if(strsSize == 0) return ret;
 
+  int shortestLen = strlen(strs[0]);
+  int shortestIndex = 0;
+  for(int i = 1; i < strsSize; i++) {
+    if(strlen(strs[i]) < shortestLen) {
+      shortestLen = strlen(strs[i]);
+      shortestIndex = i;
+    }
+  }
+  for(int c = 0; c < shortestLen; c++) {
+    for(int i = 0; i < strsSize; i++) {
+      if(strs[i][c] != strs[shortestIndex][c]) {
+        ret[c] = '\0';
+        return ret;
+      }
+    }
+    ret[c] = strs[shortestIndex][c];
+  }
+
+  ret[shortestLen] = '\0';
   return ret;
 }
 
 int main() {
-  char *ret[] = {"flower", "flow", "flight", "flowerpot"};
+  //char *ret[] = {"flower", "flow", "flight" };
+  char *ret[] = {"a"};
   int size = sizeof(ret) / sizeof(ret[0]);
   printf("size %d\n", size);
-  char *k = longestCommonPrefix(ret,4);
+  char *k = longestCommonPrefix(ret,size);
   printf("%s\n", k);
   free(k);
 
