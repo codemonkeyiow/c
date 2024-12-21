c4b929e9b7ad3549889f7cbf5d8aebcb10a1d5cd
c0d3m0nk3y <rickyoswald@gmail.com>
2024-12-21 20:38:42 +0000

    Stole it

---
+++ leetcode/0021.TODO.MergeTwoSortedLists.c
@@ -0,0 +1,37 @@
+#include <stddef.h>
+#include <stdlib.h>
+
+struct ListNode
+{
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode *mergeTwoLists(struct ListNode *list1, struct ListNode *list2)
+{
+    struct ListNode dummy;
+    dummy.next = NULL;
+    struct ListNode *cur = &dummy;
+    while (list1 != NULL && list2 != NULL)
+    {
+        if (list1->val > list2->val)
+        {
+            cur->next = list2;
+            list2 = list2->next;
+        }
+        else
+        {
+            cur->next = list1;
+            list1 = list1->next;
+        }
+        cur = cur->next;
+    }
+
+    cur->next = (list1 != NULL) ? list1 : list2;
+    return dummy.next;
+}
+
+int main()
+{
+    struct ListNode *list = (struct ListNode *)malloc(sizeof(struct ListNode));
+}
