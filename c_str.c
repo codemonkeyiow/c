977dc096978b78694513b3b25b3664baee3c2083
c0d3m0nk3y <rickyoswald@gmail.com>
2024-12-23 15:46:37 +0000

    Dynamic length character pointer

---
+++ c_str.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int str_len = 10;
+    char* c_string = malloc(sizeof(char) * (str_len + 1));
+    for(int i = 0; i < str_len; i++) {
+        *(c_string + i) = i + 64;
+    }
+    *(c_string + str_len) = '\n';
+    printf("%s\n", c_string);
+
+    return 0;
+}
